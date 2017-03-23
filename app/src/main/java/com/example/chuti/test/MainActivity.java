26a327b20b2cd3fd2329f1291ff4982bc08b1b3f
praewiiezzz <modzaasard@hotmail.com>
2017-03-23 21:00:06 +0700

    complete call status.cgi

--- app/src/main/java/com/example/chuti/test/MainActivity.java
+++ app/src/main/java/com/example/chuti/test/MainActivity.java
@@ -1,123 +1,83 @@
 package com.example.chuti.test;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends Activity {
+    private TextView textView;
 
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("gg");
+        textView = (TextView) findViewById(R.id.TextView01);
 
-        double gg = 5;
-        try {
-            gg = sendGet();
-        } catch (Exception e) {
-            gg = 9;
-            e.printStackTrace();
-        }
-        ((TextView)findViewById(R.id.textView)).setText("Test = " + gg);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public double sendGet()  {
-
-     //   String url = "http://192.168.1.20/status.cgi";
-        String url = "http://www.google.com";
-
-        URL obj = null;
-        try {
-            obj = new URL(url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection con = null;
-        try {
-            con = (HttpURLConnection) obj.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // optional default is GET
-        try {
-            con.setRequestMethod("GET");
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        }
-
-        //con.setRequestProperty("Cookie", "AIROS_SESSIONID=e02a09d3c8acda8237a374c103e5d940; path=/; domain=192.168.1.20; Expires=Tue Jan 19 2038 03:14:07 GMT+0700 (SE Asia Standard Time);");
-
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        try {
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+        //run every seconds
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override public void run() {
+                readWebpage();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }, 0, 1000);
+    }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            for (String url : urls) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url);
+                httpGet.setHeader("Cookie", "AIROS_SESSIONID=52fb8930fe14a09dc50e651f0fd87971; path=/; domain=192.168.1.20; Expires=Tue Jan 19 2038 03:14:07 GMT+0700 (SE Asia Standard Time);");
+                try {
+
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(
+                            new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return response;
         }
-        try {
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(Html.fromHtml(result));
+            Log.v("html", Html.fromHtml(result).toString());
         }
+    }
 
-        //print result
-
-        System.out.println("gg2");
-   //     System.out.println(response.toString());
-
-        return 1;
+    public void readWebpage () {
+        DownloadWebPageTask task = new DownloadWebPageTask();
+        task.execute("http://192.168.1.20/status.cgi");
 
     }
+
 
 }
